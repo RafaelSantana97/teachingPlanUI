427ef55eeeaa91e908c269a6e3c5e8d15e897629
Rafael Santana <rafaellauton@outlook.com>
2019-04-08 22:25:08 -0300

    Logando apenas com usuarios cadastrados

--- src/app/shared/classes-padrao/base-service.ts
+++ src/app/shared/classes-padrao/base-service.ts
@@ -5,7 +5,7 @@ import { WebServiceResponse, Pagination } from '../core/web-service-response.mod
 export class BaseService<T> {
     headers: HttpHeaders;
     urlBase: string = "";
-    token: string = "MeuVizinhoComeChurros|=wO'M*ZDH5+46/_YEwxCom12AnosAzuiseyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyb2RyaWdvZXNAb3V0bG9vay5jb20iLCJleHAiOjE1NTQ3NjU1NTZ9.8dzr_BaGGJyoQNfw-E5OA4m1pvboqHuvrt-pXytLoQojqnpfRB0Z6QUERdjqCnPBUu7Tvp2OipmwFTdg0m_H3w";
+    token: string = "";
     wsHandler: WebServiceResponseHandlerService<T> = new WebServiceResponseHandlerService<T>();
     httpOtions: { headers: HttpHeaders }
 
@@ -14,7 +14,7 @@ export class BaseService<T> {
         url: string
     ) {
         this.urlBase = 'http://localhost:8080/api/' + url;
-        //this.token = sessionStorage.getItem('token');
+        this.token = sessionStorage.getItem('token');
 
         this.headers = new HttpHeaders({ 'Authorization': this.token, 'Content-Type': 'application/json' });
         this.httpOtions = { headers: this.headers };
