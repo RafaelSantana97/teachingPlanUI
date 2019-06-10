dcd6d52244d2cc122bcb9a3f2a370298b639fe3c
Rafael Santana <rafaellauton@outlook.com>
2019-06-10 00:52:47 -0300

    Montando tela de concessão de permissões

---
+++ src/app/components/granting-permissions/granting-permissions.model.ts
@@ -0,0 +1,33 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BaseModel } from "src/app/shared/classes-padrao/base-model";
+
+export class GrantingPermissions implements BaseModel {
+
+    id: number;
+
+    name: string;
+    email: string;
+
+    currentAdminRole: boolean;
+    currentTeacherRole: boolean;
+    currentCoordinatorRole: boolean;
+
+    requiredAdminRole: boolean;
+    requiredTeacherRole: boolean;
+    requiredCoordinatorRole: boolean;
+
+    static createFormGroup(formBuilder: FormBuilder): FormGroup {
+        return formBuilder.group({
+            name: [null, [Validators.required]],
+            email: [null, [Validators.email, Validators.required]],
+
+            currentAdminRole: { value: false, disabled: false },
+            currentTeacherRole: { value: false, disabled: false },
+            currentCoordinatorRole: { value: false, disabled: false },
+
+            requiredAdminRole: { value: false, disabled: false },
+            requiredTeacherRole: { value: false, disabled: false },
+            requiredCoordinatorRole: { value: false, disabled: false },
+        });
+    }
+}
---
+++ src/app/components/granting-permissions/granting-permissions.module.ts
@@ -0,0 +1,35 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { DialogModule } from 'src/app/shared/modules/dialog/dialog.module';
+import { MyTableModule } from 'src/app/shared/modules/my-table/my-table.module';
+import { PageHeaderModule } from '../../shared/modules/page-header/page-header.module';
+import { PermissionModule } from 'src/app/core/manager/permission.module';
+import { GrantingPermissionsComponent } from './granting-permissions.component';
+import { GrantingPermissionsRoutingModule } from './granting-permissions-routing.module';
+import { GrantingPermissionsService } from './granting-permissions.service';
+import { SharedPipesModule } from 'src/app/shared';
+import { TranslateModule } from '@ngx-translate/core';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    DialogModule,
+    FormsModule,
+    MyTableModule,
+    PageHeaderModule,
+    PermissionModule,
+    ReactiveFormsModule,
+    SharedPipesModule,
+    GrantingPermissionsRoutingModule,
+    TranslateModule,
+  ],
+  declarations: [
+    GrantingPermissionsComponent,
+  ],
+  providers: [
+    GrantingPermissionsService
+  ]
+})
+export class GrantingPermissionsModule { }
